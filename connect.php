29029ce04333167dff571298ab3989f7b83bf01b
joshbeaman <joshbeaman@shaw.ca>
2024-02-12 15:36:10 -0800

    Changes to connect.php

--- connect.php
+++ connect.php
@@ -4,4 +4,16 @@
     $email = $_POST['email'];
 
     //Database Connection
+    $conn = new mysqli('localhost', 'root', '', 'PersonalWeb');
+    if($conn->connect_error){
+        die('Connection Failed : ' .$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into registration(firstName, lastName, email) 
+            values(?, ?, ?)");
+        $stmt->bind_param("sss", $firstName, $lastName, $email);
+        $stmt->execute();
+        echo "Registration Successfully ...";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
